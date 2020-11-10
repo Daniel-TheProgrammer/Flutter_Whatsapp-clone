89d8b21fc772c08667cf1b30b9b6c34149ed25d1
Nji Daniel <60320246+Daniel-TheProgrammer@users.noreply.github.com>
2020-11-10 12:24:38 +0000

    Update button_tap_test.dart

--- platform_channel_swift/test_driver/button_tap_test.dart
+++ platform_channel_swift/test_driver/button_tap_test.dart
@@ -1,4 +1,4 @@
-// Copyrigh The Flutter Authors. All rights reserved.
+// zvhjzThe Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
